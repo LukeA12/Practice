85c06980b008a05cdad5a5050448ff00ac07f8d0
LukeA12 <lukeauderer@yahoo.com>
2022-08-17 19:49:03 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("WHY WONT VS CODE WORK");
+        for(int i = 0; i < 10; i++){
+            System.out.println("ECLIPSE IS BETTR");
+        }
+        for(int j = 0; j < 10; j++){
+            System.out.println("Wait, is it workin now?????????????????");
+        }
+    }
+}
